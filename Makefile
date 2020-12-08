c6822a2dc34d84c450a98a3dc7650aebb5cdaa28
DmitryMurashov <dmtr.murashov@gmail.com>
2020-12-08 23:42:01 +0300

    Project: configuration files, gitignore

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 %:
 	cp -r _carcass $@
+	sed -i 's/__CARCASS__/$@/g' $@/CMakeLists.txt
+	sed -i 's/__CARCASS__/$@/g' $@/Makefile
--- merging_leaderboard/main.cpp
+++ merging_leaderboard/main.cpp
@@ -1,13 +1,29 @@
+#include <iostream>
 #include <vector>
+#include <iterator>
+#include <algorithm>
 
 using namespace std;
 
-void fill(vector<unsigned> &leaders, vector<unsigned> &player)
-{
+using VecResults = std::vector<unsigned>;
 
+void fill(VecResults &leaders, VecResults &player)
+{
+	unsigned n;
+
+	cin >> n;
+	leaders.reserve(n);
+	leaders.assign(istream_iterator<unsigned>(cin), istream_iterator<unsigned>());
+
+	cin >> n;
+	player.reserve(n);
+	player.assign(istream_iterator<unsigned>(cin), istream_iterator<unsigned>());
 }
 
 int main(void)
 {
-    return 0;
+	VecResults leaders;
+	VecResults players;
+
+	fill(leaders, players);
 }
